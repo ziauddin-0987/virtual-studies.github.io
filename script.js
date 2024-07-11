762d27e67431d7a9d0fe187c497f740346e2b67c
ziauddin-0987 <ziauddink100@gmail.com>
2024-07-12 04:01:46 +0500

    this my porject navbar

---
+++ script.js
@@ -0,0 +1,31 @@
+// const navBtn = document.getElementById("navIcon");
+
+// let showBtn = 0;
+// navBtn.addEventListener("click", () => {
+//   if (showBtn === 0) {
+//     console.log("on btn");
+//     showBtn = 1;
+//   } else if (showBtn === 1) {
+//     console.log("of btn");
+//     showBtn = 0;
+//   }
+// });
+// document.getElementById('mydiv').className += ' newClass';
+// const Obj = {
+//   myName: "ziauddin",
+//   myMarks: 94.6,
+//   marksfunc: function () {
+//     console.log("this is my marks", this.myMarks);
+//   },
+// };
+
+const obj = {
+  jaan: function () {
+    console.log("ziauddin");
+  },
+};
+const moiz = {
+  myjaan: "moiz",
+};
+
+maiz.__proto__ = obj;
