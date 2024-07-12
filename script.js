51ab3b85cfb538aa54a58389384c4dcd64736a04
ziauddin-0987 <ziauddink100@gmail.com>
2024-07-13 02:13:31 +0500

    changes code in navBar

--- script.js
+++ script.js
@@ -1,31 +1,22 @@
-// const navBtn = document.getElementById("navIcon");
+const navBtn = document.getElementById("navIcon");
+const navClass = document.getElementById("navList").classList;
 
-// let showBtn = 0;
-// navBtn.addEventListener("click", () => {
-//   if (showBtn === 0) {
-//     console.log("on btn");
-//     showBtn = 1;
-//   } else if (showBtn === 1) {
-//     console.log("of btn");
-//     showBtn = 0;
-//   }
-// });
-// document.getElementById('mydiv').className += ' newClass';
-// const Obj = {
-//   myName: "ziauddin",
-//   myMarks: 94.6,
-//   marksfunc: function () {
-//     console.log("this is my marks", this.myMarks);
-//   },
-// };
-
-const obj = {
-  jaan: function () {
-    console.log("ziauddin");
-  },
-};
-const moiz = {
-  myjaan: "moiz",
-};
-
-maiz.__proto__ = obj;
+let showBtn = 0;
+navBtn.addEventListener("click", () => {
+  if (showBtn === 0) {
+    console.log("on btn");
+    navClass.add("navList");
+    showBtn = 1;
+  } else if (showBtn === 1) {
+    showBtn = 0;
+  }
+});
+function zia() {
+  const navClass = document.getElementById("navList");
+  console.log("on btn");
+  navClass.classList.toggle("navList");
+}
+function zia() {
+  console.log("of btn");
+  document.getElementById("navClass").classList.toggle("myStyle");
+}
